8e142cee779b4bcbbbb6cf6b827edd4c9981e2e6
sooqa1nya <sooqa1nya@gmail.com>
2026-03-11 00:34:53 +0700

    no error if user block bot

--- src/services/remnawave/webhooks.ts
+++ src/services/remnawave/webhooks.ts
@@ -20,21 +20,23 @@ export const serverFastify = () => {
 
             const [sub] = await getProfile(body.data.uuid);
 
-            if (body.event == 'user.expired') {
-                await bot.api.sendMessage({
-                    chat_id: body.data.telegramId,
-                    text: `🛑 Доступ ограничен! Срок вашей подписки <code>${body.data.username}</code> истек. Продлите её сейчас, чтобы оставаться в безопасности и сохранить анонимность. 🔐⚡️`,
-                    parse_mode: 'HTML',
-                    reply_markup: await extendSubKeyboard(sub.id)
-                });
-            } else if (body.event == 'user.expires_in_24_hours') {
-                await bot.api.sendMessage({
-                    chat_id: body.data.telegramId,
-                    text: `⏳ Внимание! Ваша подписка <code>${body.data.username}</code> истекает через 1 день. Продлите её заранее, чтобы не потерять защиту и сохранить анонимность. 🔐⚡️`,
-                    parse_mode: 'HTML',
-                    reply_markup: await extendSubKeyboard(sub.id)
-                });
-            }
+            try {
+                if (body.event == 'user.expired') {
+                    await bot.api.sendMessage({
+                        chat_id: body.data.telegramId,
+                        text: `🛑 Доступ ограничен! Срок вашей подписки <code>${body.data.username}</code> истек. Продлите её сейчас, чтобы оставаться в безопасности и сохранить анонимность. 🔐⚡️`,
+                        parse_mode: 'HTML',
+                        reply_markup: await extendSubKeyboard(sub.id)
+                    });
+                } else if (body.event == 'user.expires_in_24_hours') {
+                    await bot.api.sendMessage({
+                        chat_id: body.data.telegramId,
+                        text: `⏳ Внимание! Ваша подписка <code>${body.data.username}</code> истекает через 1 день. Продлите её заранее, чтобы не потерять защиту и сохранить анонимность. 🔐⚡️`,
+                        parse_mode: 'HTML',
+                        reply_markup: await extendSubKeyboard(sub.id)
+                    });
+                }
+            } catch { }
         }
 
         return 'ok';
